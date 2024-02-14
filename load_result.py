1451925cf259b3b4f5566f8d5378501e5c02c549
kazumanakata <121463877+kazumanakata@users.noreply.github.com>
2024-02-14 10:00:48 +0900

    Update load_result.py

--- load_result.py
+++ load_result.py
@@ -26,24 +26,3 @@ show_fig(list5)
 show_fig(list6)
 show_fig(list7)
 show_fig(list8)
-
-
-'''
-plt.plot(np.arange(len(list1)), list1)
-plt.plot(np.arange(len(list1)), list1)
-plt.plot(np.arange(len(list1)), list1)
-plt.plot(np.arange(len(list1)), list1)
-plt.plot(np.arange(len(list1)), list1)
-plt.plot(np.arange(len(list1)), list1)
-plt.plot(np.arange(len(list1)), list1)
-'''
-# plt.fill_between(np.arange(200), list1_mean-list1_std, list1_mean+list1_std, alpha=0.2)
-# plt.fill_between(np.arange(200), list2_mean-list2_std, list2_mean+list2_std, alpha=0.2)
-# plt.fill_between(np.arange(200), list3_mean-list3_std, list3_mean+list3_std, alpha=0.2)
-# plt.fill_between(np.arange(200), list4_mean-list4_std, list4_mean+list4_std, alpha=0.2)
-# plt.fill_between(np.arange(200), list5_mean-list5_std, list5_mean+list5_std, alpha=0.2)
-# plt.fill_between(np.arange(200), list5_mean-list6_std, list6_mean+list6_std, alpha=0.2)
-# plt.fill_between(np.arange(200), list5_mean-list7_std, list7_mean+list7_std, alpha=0.2)
-
-# plt.legend()
-# plt.show()
